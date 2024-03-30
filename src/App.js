ece75a93a006839d33027a9d9496e68c8773608f
empotts <ethanmpotts@gmail.com>
2024-03-30 16:48:12 -0500

    adding new columns broken

--- src/App.js
+++ src/App.js
@@ -4,58 +4,153 @@ import React, { useState } from 'react';
 import fbsTeams from './fbsTeams.json';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
+const grid = 18;
 
 
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
 
+  destClone.splice(droppableDestination.index, 0, removed);
 
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  return result;
+};
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
 
 
 
 function App() {
-  const [teams, setTeams] = useState(fbsTeams);
+  const [teams, setTeams] = useState([fbsTeams]);
 
-  const handleOnDragEnd = (result) => {
-    if (!result.destination) return;
-    const items = Array.from(teams);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    setTeams(items);
-  }
+
+  const onDragEnd = result => {
+    const { source, destination } = result;
+  
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
+  
+    // Convert droppableId back to a number
+    const sourceIndex = Number(source.droppableId);
+    const destinationIndex = Number(destination.droppableId);
+  
+    if (sourceIndex === destinationIndex) {
+      // Ensure the list is defined
+      if (teams[sourceIndex]) {
+        const items = reorder(
+          teams[sourceIndex],
+          source.index,
+          destination.index
+        );
+  
+        let newState = [...teams];
+        newState[sourceIndex] = items;
+  
+        setTeams(newState);
+      }
+    } else {
+      // Ensure both lists are defined
+      if (teams[sourceIndex] && teams[destinationIndex]) {
+        const result = move(
+          teams[sourceIndex],
+          teams[destinationIndex],
+          source,
+          destination
+        );
+  
+        setTeams(result);
+      }
+    }
+  };
 
 
 
   return (
     <div className="App">
-
-
       <h1>FBS Teams</h1>
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="teams">
-          {(provided) => (
-            <ul className="teams" {...provided.droppableProps} ref={provided.innerRef}>
-              {teams.map(({ school, logoPath, id }, index) => {
-                //console.log(school,id, index);
-                return (
-                  <Draggable key={id} draggableId={id} index={index}>
-                    {(provided) => (
-                      <li id={id} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                        <div className="team-thumb">
-                          <img src={'/' + logoPath} alt={`${school} Thumb`} />
+      <button
+        type="button"
+        onClick={() => {
+          setTeams([...teams, []]);
+        }}
+      >
+        Add new group
+      </button>
+      <div style={{ display: "flex" }}>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {teams.map((teamList, i) => (
+            <Droppable key={i} droppableId={`column-${i}`}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                  {...provided.droppableProps}
+                >
+                  {teamList.map(({ school, logoPath, id }, index) => (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-around"
+                            }}
+                          >
+                            <div className="team-thumb">
+                              <img src={'/' + logoPath} alt={`${school} Thumb`} />
+                            </div>
+                            <p>{school}</p>
+                           
+                          </div>
                         </div>
-                        <p>
-                          {school}
-                        </p>
-                      </li>
-                    )}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-
-            </ul>
-          )}
-        </Droppable>
-      </DragDropContext>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </DragDropContext>
+      </div>
     </div>
   );
 }
