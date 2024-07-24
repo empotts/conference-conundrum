731fa7ae1e88a748cd578883e5aeb5f2e1df9ca1
empotts <ethanmpotts@gmail.com>
2024-07-23 21:10:41 -0500

    Actual App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,8 @@ import fbsTeams from "./fbsTeams.json";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import Team from "./components/team";
 
-
-const getItemStyle = (isDragging, draggableStyle) => ({
-  // some basic styles to make the items look a bit nicer
-  userSelect: "none",
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
+function App() {
+  const [teams, setTeams] = useState(fbsTeams);
 
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
@@ -59,7 +55,6 @@ const getItemStyle = (isDragging, draggableStyle) => ({
           )}
         </Droppable>
       </DragDropContext>
-
     </div>
   );
 }
